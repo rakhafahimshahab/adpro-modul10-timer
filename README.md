066678a4629267368dbbac5c00c81d268113e1cc
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-08 16:24:09 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Experiment 1.2: Understanding how it works.
 <img src="images/image1.png">
 "hey hey modified" came first because the we put it outside the asynchronous task.
+
+## Experiment 1.3: Multiple Spawn and removing drop
+### drop
+<img src="images/image2.png">
+
+### drop removed
+<img src="images/image3.png">
+
+if drop spawn is removed the task will never stop on going it will keep going until there's a drop.
--- src/main.rs
+++ src/main.rs
@@ -104,11 +104,23 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Rakha's Computer: done!");
     });
+    spawner.spawn(async {
+        println!("Rakha's Computer: howdy!2");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Rakha's Computer: done!2");
+    });
+    spawner.spawn(async {
+        println!("Rakha's Computer: howdy!3");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Rakha's Computer: done!3");
+    });
     println!("Rakha's Computer: hey hey modified");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
