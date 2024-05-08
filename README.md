9558cc2667172ff15df5a52cd79f93d622ecc979
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-08 16:13:50 +0700

    Experiment 1.2: Understanding how it works.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reflection
+
+## Experiment 1.2: Understanding how it works.
+<img src="images/image1.png">
+"hey hey modified" came first because the we put it outside the asynchronous task.
--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Rakha's Computer: done!");
     });
+    println!("Rakha's Computer: hey hey modified");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
